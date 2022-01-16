c27019a7840fbe074f37466d1046a324e8414494
cherlux82 <icc.vicente.garcia@hotmail.com>
2022-01-15 19:23:32 -0600

    Clase 120, RequestParam

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,10 +1,9 @@
 package com.luv2code.springdemo.mvc;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -19,13 +18,11 @@ public class HelloWorldController {
 	}
 
 	@RequestMapping("/processForm")
-	public String letsShoutDude(HttpServletRequest request, Model model) {
-
-		String name = request.getParameter("studentName");
+	public String letsShoutDude(@RequestParam("studentName") String name, Model model) {
 
 		name = name.toUpperCase();
 
-		String result = "Yo! " + name;
+		String result = "Hey my friend! " + name;
 		model.addAttribute("message", result);
 
 		return "helloworld";
