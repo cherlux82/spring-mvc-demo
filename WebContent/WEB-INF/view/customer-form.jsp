31f3958df9dab137441910cdf043f90dc0c6b895
cherlux82 <icc.vicente.garcia@hotmail.com>
2022-01-15 22:51:48 -0600

    Clase 156 - Validacion de minimos y maximos

--- WebContent/WEB-INF/view/customer-form.jsp
+++ WebContent/WEB-INF/view/customer-form.jsp
@@ -22,6 +22,9 @@
 	* Last Name: <form:input path="lastName" />
 		<form:errors path="lastName" cssClass="error" />
 		<br>
+	* Free Passes: <form:input path="freePasses" />
+		<form:errors path="freePasses" cssClass="error" />
+		<br>
 		<input type="submit" value="Submit">
 	</form:form>
 </body>
--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -1,14 +1,20 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class Customer {
 
 	private String firstName;
-	@NotNull
-	@Size(min = 1, message = "Es requerido")
+	@NotNull(message = "Es requerido")
+	@Size(min = 1, message = "Tamano min 1")
 	private String lastName;
+
+	@Min(value = 0, message = "Debe ser mayor a cero")
+	@Max(value = 10, message = "Debe ser menor a diez")
+	private int freePasses;
 
 	public String getFirstName() {
 		return firstName;
@@ -26,9 +32,17 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+
 	@Override
 	public String toString() {
-		return "Customer [firstName=" + firstName + ", lastName=" + lastName + "]";
+		return "Customer [firstName=" + firstName + ", lastName=" + lastName + ", freePasses=" + freePasses + "]";
 	}
 
 }
